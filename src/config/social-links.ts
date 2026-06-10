8a4b73089686dfe7cea15bffce80bd4394abe66f
[dyad] <leftis.pro@gmail.com>
2026-06-10 09:38:59 +0300

    [dyad] Add Facebook social link

--- src/config/social-links.ts
+++ src/config/social-links.ts
@@ -7,4 +7,8 @@ export const socialLinks = [
     label: "Reddit",
     href: "https://reddit.com/user/Possible_Loss7625",
   },
+  {
+    label: "Facebook",
+    href: "https://www.facebook.com/profile.php?id=61590356836066",
+  },
 ] as const;
